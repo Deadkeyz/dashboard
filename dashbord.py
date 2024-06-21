f9398aa77814b41b34611c39fca025d6bb726b06
Deadkeyz <151540063+Deadkeyz@users.noreply.github.com>
2024-06-21 19:23:47 -0400

    Update dashbord.py

--- dashbord.py
+++ dashbord.py
@@ -1,3 +1,4 @@
+from sklearn.discriminant_analysis import StandardScaler
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -38,19 +39,20 @@ def main():
             de les préparer, de construire des modèles de prédiction et d'évaluer leurs performances. 
             Dans ce projet, nous allons explorer et analyser un ensemble de données de 5960 observations avec 13 variables, afin de prédire la probabilité de défaut de crédit. Les données contiennent des informations sur les prêts, les hypothèques, les emplois, et d'autres variables financières et démographiques.        
         """)
-
+        st.image("Franck-Vermet.png", use_column_width=True)
     elif choice == "Compréhension des données":
         if uploaded_file is not None:
                 data = load_data(uploaded_file)
                 st.header("Aperçu des données")
                 st.write(data.info())
                 st.write(data.head())
+                
                 st.write("""
                     ## Description des Données
 
                     Les données contiennent les variables suivantes:
 
-                    - **BAD** : Indicateur de défaut (1 = défaut, 0 = non défaut)
+                    - **BAD** : Indicateur de défaut (1 = En défaut, 0 = Conforme)
                     - **LOAN** : Montant du prêt
                     - **MORTDUE** : Montant dû sur l'hypothèque existante
                     - **VALUE** : Valeur de la propriété actuelle
@@ -85,7 +87,182 @@ def main():
                     * On a Indicateur de défaut  ici BAD contient **0** valeurs manquantes.
                     * On a Montant du prêt ici LOAN contient **0** valeurs manquantes.     
                           """)
-                st.write("Gestion des valeurs manquantes")
+                if 'BAD' in data.columns and data['BAD'].dtype in ['int64', 'float64']:
+                    bins = [-1, 0, 1]  # Ajustez ces seuils selon vos besoins
+                    labels = ['Conforme', 'En défaut']
+                    data['BAD'] = pd.cut(data['BAD'], bins=bins, labels=labels, right=True)
+                    data['BAD'] = data['BAD'].astype(str)
+                    st.write("Gestion des valeurs manquantes")
+                    data['REASON'].fillna(data['REASON'].mode()[0], inplace=True)
+                    data['JOB'].fillna(data['JOB'].mode()[0], inplace=True)
+                    data['MORTDUE'].fillna(data['MORTDUE'].median(), inplace=True)
+                    data['VALUE'].fillna(data['VALUE'].median(), inplace=True)
+                    data['YOJ'].fillna(data['YOJ'].median(), inplace=True)
+                    data['DEROG'].fillna(data['DEROG'].median(), inplace=True)
+                    data['DELINQ'].fillna(data['DELINQ'].median(), inplace=True)
+                    data['CLAGE'].fillna(data['CLAGE'].median(), inplace=True)
+                    data['NINQ'].fillna(data['NINQ'].median(), inplace=True)
+                    data['CLNO'].fillna(data['CLNO'].median(), inplace=True)
+                    data['DEBTINC'].fillna(data['DEBTINC'].median(), inplace=True)
+
+                    st.write("Données après remplacement des valeurs manquantes par le mode et la mediane :")
+                    st.write(data.head())
+                    st.header("Analyse Exploratoire des Données")
+
+                    st.subheader("Statistiques descriptives")
+                    st.write(data.describe())
+
+                    st.subheader("Distribution des variables (Univarié)")
+                    selected_col = st.selectbox("Sélectionnez une colonne pour analyser sa distribution", data.columns)
+                    if 'BAD' in selected_col:
+                        fig_bad = px.histogram(data, x='BAD', title='Distribution de BAD')
+                        st.plotly_chart(fig_bad)
+                        comment_bad = "Nous remarquons qu'il y a beaucoup plus de personnes conforme que de personnes en defaut. Nous avons environs 5000  individus qui sont conforme et moins de 1500 qui sont en defaut ."
+                        st.write(f"Commentaire : {comment_bad}")
+
+                    if 'LOAN' in selected_col:
+                        fig_bad = px.histogram(data, x='LOAN', title='Distribution de LOAN')
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+                            "La distribution de la variable LOAN est asymétrique à droite, "
+                            "indiquant une concentration élevée de prêts dans les gammes inférieures avec un pic entre 10k et 20k. "
+                            "Les valeurs s'étendent d'environ 5k à plus de 80k, mais les prêts au-delà de 60k sont très peu nombreux. "
+                            "Cette distribution peut indiquer que la majorité des clients optent pour des prêts de petites à moyennes sommes, "
+                            "ce qui pourrait être dû à une politique de prudence face aux risques associés à de grands montants prêtés."
+                        )
+                        st.write(f"Commentaire : {comment_bad}")
+
+                    if 'MORTDUE' in selected_col:
+                        fig_bad = px.histogram(data, x='MORTDUE', title="Distribution des Montant dû sur l'hypothèque existante")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+                        "La distribution est fortement concentrée autour de valeurs basses avec un pic marqué près de 50k, "
+                        "indiquant que la majorité des hypothèques dans cet ensemble ont des montants dus faibles. "
+                        "Bien que la distribution s'étende à des valeurs plus élevées, dépassant 350k, ces cas sont nettement moins fréquents. "
+                        "Cette caractéristique peut suggérer une moindre vulnérabilité globale au défaut sur ces hypothèques, "
+                        "mais aussi pointer vers des risques significatifs liés aux rares montants élevés."
+                    )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'VALUE' in selected_col:
+                        fig_bad = px.histogram(data, x='VALUE', title="Distribution de la Valeur de la propriété actuelle")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+                        "La distribution montre une forte concentration des valeurs autour de moins de 200k, avec un pic marqué près de 100k, "
+                        "suggérant que la majorité des propriétés ont une valeur relativement modeste. "
+                        "La distribution s'étend jusqu'à des valeurs supérieures, atteignant 800k, mais avec une fréquence nettement décroissante, "
+                        "indiquant que les propriétés de très haute valeur sont rares dans cet ensemble de données. "
+                        "Cette répartition des valeurs peut influencer la capacité des emprunteurs à obtenir des prêts plus élevés et pourrait "
+                        "être indicative de la stabilité financière globale des emprunteurs dans l'ensemble de données."
+        )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'REASON' in selected_col:
+                        fig_bad = px.histogram(data, x='REASON', title="Distribution de la Raison de la demande de prêt")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = "Nous remarquons qu'il y a une forte demande de pret pour des raisons de consolidation de dettes que pour une amélioration de l'habitat . Nous avons entre autre plus de 4000 individus contre 1500 ."
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'JOB' in selected_col:
+                        fig_bad = px.histogram(data, x='JOB', title="Distribution du Type d'emploi")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+                        "La catégorie 'Other' domine nettement la distribution, ce qui indique que la majorité des emprunteurs dans l'ensemble de données "
+                        "ne rentrent pas dans les catégories d'emploi traditionnelles listées ou travaillent dans des secteurs variés. "
+                        "Les professions 'Self' (indépendants) et 'ProfExe' (professionnels exécutifs) sont également bien représentées, "
+                        "suggérant une présence significative d'individus ayant probablement un niveau de revenu et de stabilité financière plus élevé. "
+                        "Les catégories 'Office' et 'Sales' sont moins représentées, ce qui pourrait indiquer des niveaux de revenus inférieurs ou "
+                        "une stabilité d'emploi moindre comparativement aux autres groupes."
+                    )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'YOJ' in selected_col:
+                        fig_bad = px.histogram(data, x='YOJ', title='Distribution de YOJ')
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "La distribution des années sur le poste actuel montre un pic significatif pour les emprunteurs avec peu d'ancienneté, "
+        "surtout entre 0 et 5 ans. Cela pourrait refléter une instabilité professionnelle pour une partie des emprunteurs, "
+        "ce qui est un facteur à considérer dans l'évaluation du risque de crédit."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+
+                    if 'DEROG' in selected_col:
+                        fig_bad = px.histogram(data, x='DEROG', title='Distribution de DEROG')
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "La distribution montre que la majorité des emprunteurs n'ont pas de dérogations sur leur dossier de crédit. "
+        "Les cas avec des dérogations sont très rares, signalant des exceptions plutôt que la norme. "
+        "Cela suggère un profil de risque généralement faible pour la majorité des emprunteurs."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+
+                    if 'DELINQ' in selected_col:
+                        fig_bad = px.histogram(data, x='DELINQ', title="Distribution DELINQ")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "La distribution indique que la plupart des emprunteurs n'ont aucun retard de paiement, "
+        "avec une présence minoritaire d'emprunteurs ayant des incidents. Cela peut être interprété comme un signe de bonne santé financière globale."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'CLAGE' in selected_col:
+                        fig_bad = px.histogram(data, x='CLAGE', title="Distribution de la CLAGE")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "Le graphique montre que de nombreux emprunteurs possèdent des lignes de crédit bien établies, "
+        "avec un pic entre 100 et 200 mois. Cette ancienneté peut être favorable pour l'évaluation de leur crédibilité."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'NINQ' in selected_col:
+                        fig_bad = px.histogram(data, x='NINQ', title="Distribution de la Raison de la demande de prêt")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "Cette variable montre que la majorité des emprunteurs ont peu ou pas de nouvelles enquêtes de crédit, "
+        "ce qui suggère une activité de crédit modérée et potentiellement moins de risque de surendettement."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'CLNO' in selected_col:
+                        fig_bad = px.histogram(data, x='CLNO', title="Distribution du CLNO")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "La plupart des emprunteurs gèrent un nombre modéré de lignes de crédit, avec un pic notable entre 10 et 20. "
+        "Cela indique une gestion de crédit relativement diversifiée sans aller vers une prolifération excessive."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+                    if 'DEBTINC' in selected_col:
+                        fig_bad = px.histogram(data, x='DEBTINC', title="Distribution du DEBTINC")
+                        st.plotly_chart(fig_bad)
+                        comment_bad = (
+        "La distribution du ratio dette/revenu est extrêmement concentrée autour de faibles valeurs, "
+        "montrant que la majorité des emprunteurs ont un faible endettement par rapport à leur revenu, "
+        "ce qui est un indicateur positif pour la stabilité financière."
+    )
+                        st.write(f"Commentaire : {comment_bad}")
+
+
+                    st.subheader("Relations entre les variables (Bivarié)")
+                    var1 = st.selectbox("Sélectionnez la première colonne", data.columns)
+                    var2 = st.selectbox("Sélectionnez la deuxième colonne", data.columns)
+                if var1 and var2:
+                    if data[var1].dtype in ['int64', 'float64'] and data[var2].dtype == 'object':
+                        fig = px.box(data, x=var2, y=var1, title=f"Relation entre {var2} et {var1}")
+                    elif data[var1].dtype == 'object' and data[var2].dtype in ['int64', 'float64']:
+                        fig = px.box(data, x=var1, y=var2, title=f"Relation entre {var1} et {var2}")
+                    elif data[var1].dtype == 'object' and data[var2].dtype == 'object':
+                        fig = px.bar(data, x=var1, color=var2, title=f"Relation entre {var1} et {var2}")
+                    else:
+                        fig = px.scatter(data, x=var1, y=var2, title=f"Relation entre {var1} et {var2}", marginal_y="violin", marginal_x="box")
+                    st.plotly_chart(fig)
+                    comment_bi = st.text_area(f"Commentaire sur la relation entre {var1} et {var2}")
+                    st.write(f"Votre commentaire : {comment_bi}")
+
+        else:
+               st.warning("Veuillez uploader un fichier CSV pour voir l'aperçu des données.")
+
+
+    elif choice == "Préparation des données":
+        if uploaded_file is not None:
+            data = load_data(uploaded_file)
+            if 'BAD' in data.columns and data['BAD'].dtype in ['int64', 'float64']:
+                bins = [-1, 0, 1]  # Ajustez ces seuils selon vos besoins
+                labels = ['Conforme', 'En défaut']
+                data['BAD'] = pd.cut(data['BAD'], bins=bins, labels=labels, right=True)
+                data['BAD'] = data['BAD'].astype(str)
                 data['REASON'].fillna(data['REASON'].mode()[0], inplace=True)
                 data['JOB'].fillna(data['JOB'].mode()[0], inplace=True)
                 data['MORTDUE'].fillna(data['MORTDUE'].median(), inplace=True)
@@ -97,40 +274,25 @@ def main():
                 data['NINQ'].fillna(data['NINQ'].median(), inplace=True)
                 data['CLNO'].fillna(data['CLNO'].median(), inplace=True)
                 data['DEBTINC'].fillna(data['DEBTINC'].median(), inplace=True)
-
-                st.write("Données après remplacement des valeurs manquantes par le mode et la mediane :")
-                st.write(data.head())
-
-        else:
-               st.warning("Veuillez uploader un fichier CSV pour voir l'aperçu des données.")
-
-
-    elif choice == "Préparation des données":
-        if uploaded_file is not None:
-            data = load_data(uploaded_file)
-            st.header("Analyse Exploratoire des Données")
-
-            st.subheader("Statistiques descriptives")
-            st.write(data.describe())
-
-            st.subheader("Distribution des variables (Univarié)")
-            columns = data.columns
-            for col in columns:
-                fig = px.histogram(data, x=col, marginal="box", title=f"Distribution de {col}")
-                st.plotly_chart(fig)
-
-            st.subheader("Relations entre les variables (Bivarié)")
-            target_column = st.selectbox("Sélectionnez la colonne cible pour les graphiques bivariés", data.columns)
-            for col in columns:
-                if data[col].dtype in ['int64', 'float64'] and col != target_column:
-                    fig = px.scatter(data, x=col, y=target_column, title=f"Relation entre {col} et {target_column}", marginal_y="violin", marginal_x="box")
-                    st.plotly_chart(fig)
+            
+            
         else:
             st.warning("Veuillez uploader un fichier CSV pour analyser les données.")
 
     elif choice == "Modélisation et évaluation":
         if uploaded_file is not None:
             data = load_data(uploaded_file)
+            data['REASON'].fillna(data['REASON'].mode()[0], inplace=True)
+            data['JOB'].fillna(data['JOB'].mode()[0], inplace=True)
+            data['MORTDUE'].fillna(data['MORTDUE'].median(), inplace=True)
+            data['VALUE'].fillna(data['VALUE'].median(), inplace=True)
+            data['YOJ'].fillna(data['YOJ'].median(), inplace=True)
+            data['DEROG'].fillna(data['DEROG'].median(), inplace=True)
+            data['DELINQ'].fillna(data['DELINQ'].median(), inplace=True)
+            data['CLAGE'].fillna(data['CLAGE'].median(), inplace=True)
+            data['NINQ'].fillna(data['NINQ'].median(), inplace=True)
+            data['CLNO'].fillna(data['CLNO'].median(), inplace=True)
+            data['DEBTINC'].fillna(data['DEBTINC'].median(), inplace=True)
             st.header("Modélisation")
             target_column = st.selectbox("Sélectionnez la colonne cible", data.columns)
             X = data.drop(columns=[target_column])
@@ -141,7 +303,7 @@ def main():
 
             numeric_transformer = Pipeline(steps=[
                 ('imputer', SimpleImputer(strategy='median')),
-                ('scaler', RobustScaler())
+                ('scaler', StandardScaler())
             ])
 
             categorical_transformer = Pipeline(steps=[
@@ -207,5 +369,7 @@ def main():
             st.warning("Veuillez uploader un fichier CSV pour modéliser les données.")
     
 
+
 if __name__ == '__main__':
     main()
+
